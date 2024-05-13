f5ebd18a947fd22a4bb5e858ed45f34166b1abe5
YiC200333 <huangyi200333@outlook.com>
2024-05-13 18:38:50 +1200

    XConomy V2.25.15
    1、Fix problem

--- XConomy-Core/src/main/java/me/yic/xconomy/AdapterManager.java
+++ XConomy-Core/src/main/java/me/yic/xconomy/AdapterManager.java
@@ -26,6 +26,7 @@ import me.yic.xconomy.info.SyncChannalType;
 import me.yic.xconomy.lang.MessagesManager;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
@@ -49,13 +50,12 @@ public class AdapterManager {
     }
 
     public static List<String> get_Tab_PlayerList(){
-        Tab_PlayerList.removeIf(Objects::isNull);
         return Tab_PlayerList;
     }
 
     public static void remove_Tab_PlayerList(String name){
         for (int i = 0; i < Tab_PlayerList.size(); i++) {
-            if (Tab_PlayerList.get(i).equals(name)) {
+            if (Tab_PlayerList.get(i) == null || Tab_PlayerList.get(i).equals(name)) {
                 Tab_PlayerList.remove(Tab_PlayerList.get(i));
             }
         }
